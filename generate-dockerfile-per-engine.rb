84d70f6d89aabb2940f3fc90abe0dace54412dd5
Simon Harrer <simon.harrer@gmail.com>
2015-09-14 13:44:16 +0200

    Newer betsy docker scripts, more stable betsy and more engines that work on linux

--- generate-dockerfile-per-engine.rb
+++ generate-dockerfile-per-engine.rb
@@ -1,7 +1,24 @@
-engines = ["ode", "bpel-g", "orchestra"]
+class Engine
+	attr_accessor :name, :type
 
-def dockerfile(engine) 
-"""# This is a Dockerfile to create an image with betsy and the engine #{engine} installed.
+	def initialize(name, type)
+		@name = name
+		@type = type
+	end
+end
+
+engines = [
+	Engine.new("ode", :bpel),
+	Engine.new("bpelg", :bpel),
+	Engine.new("orchestra", :bpel),
+	Engine.new("active-bpel", :bpel),
+	Engine.new("activiti", :bpmn),
+	Engine.new("camunda720", :bpmn),
+	Engine.new("camunda730", :bpmn)
+]
+
+def dockerfile(engine)
+"""# This is a Dockerfile to create an image with betsy and the engine #{engine.name} installed.
 
 # Version 1.0
 
@@ -9,15 +26,15 @@ FROM betsy
 
 MAINTAINER Simon Harrer (simon.harrer@uni-bamberg.de)
 
-# install #{engine}
-RUN ./betsy engine #{engine} install
+# install #{engine.name}
+RUN ./betsy engine #{engine.name} install
 """
 end
 
 def setup_shell_script(engine)
 """./setup-betsy
 
-docker build --tag='betsy-#{engine}' --file=image/Dockerfile_#{engine} image 
+docker build --tag='betsy-#{engine.name}' --file=image/Dockerfile_#{engine.name} image
 """
 end
 
@@ -25,12 +42,12 @@ def start_shell_script(engine)
 """#!/bin/bash
 source common.sh
 
-./setup-#{engine}
+./setup-#{engine.name}
 
-docker run betsy-#{engine} sh betsy bpel #{engine} --use-installed-engine \"$*\"
+docker run betsy-#{engine.name} sh betsy #{engine.type} --use-installed-engine #{engine.name} \"$*\"
 
 params=`echo \"$*\" | tr ' =' '_'`
-folder=results/betsy-bpel-#{engine}-$params-`date +%s`
+folder=results/betsy-bpel-#{engine.name}-$params-`date +%s`
 container=`docker ps -alq`
 
 extractLogs \"$container\" \"$folder\"
@@ -41,8 +58,8 @@ end
 
 engines.each do |engine|
 
-	File.write("image/Dockerfile_#{engine}", dockerfile(engine))
-	File.write("setup-#{engine}", setup_shell_script(engine))
-	File.write("betsy-#{engine}", start_shell_script(engine))
+	File.write("image/Dockerfile_#{engine.name}", dockerfile(engine))
+	File.write("setup-#{engine.name}", setup_shell_script(engine))
+	File.write("betsy-#{engine.name}", start_shell_script(engine))
 
 end
